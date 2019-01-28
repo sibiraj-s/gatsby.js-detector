185cc64dce3c5e230a5b67a72f40681401bfb886
sibiraj-s <dev.sibiraj@outlook.com>
2019-01-29 01:05:11 +0530

    chore: exit pre-release script with exit code

--- scripts/pre-release.js
+++ scripts/pre-release.js
@@ -6,5 +6,5 @@ const pkgJson = require('../package.json');
 if (manifestJson.version !== pkgJson.version) {
   // eslint-disable-next-line no-console
   console.log(chalk.red('Error: Version specified in \'package.json\' and \'manifest.json\' doesn\'t match'));
-  process.exit();
+  process.exit(1);
 }
